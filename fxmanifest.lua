8b83234073bb31aecc235f01234b6f8aa0360d75
vNuque <finn.sendelbach@gmail.com>
2026-08-07 18:45:03 +0200

    First upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name 'bl_copnet'
+author 'BlackLeaf'
+description 'CopNet Bridge – Duty, GPS, Personen- & Fahrzeug-Sync'
+version '1.0.0'
+
+shared_scripts {
+  'config.lua',
+}
+
+client_scripts {
+  'client/main.lua',
+}
+
+server_scripts {
+  '@oxmysql/lib/MySQL.lua',
+  'server/http.lua',
+  'server/sync.lua',
+  'server/duty.lua',
+  'server/main.lua',
+}
+
+dependencies {
+  'es_extended',
+  'oxmysql',
+}
